68821f9de4696a5769f9d7f667d4990fa79da136
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-05 19:26:33 +0200

    add folder pcs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import {createRoot}from 'react-dom/client'
 import './Home/index.css'
+import Pcs from'./pcs/pcs'
 import Header from'./Home/header'
 const root= createRoot(document.getElementById('root'))
 root.render(
